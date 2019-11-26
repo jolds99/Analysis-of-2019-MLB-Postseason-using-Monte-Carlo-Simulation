adab0b187c2ac6412bb623bf7a2665de8fe14521
quinnjohnson303 <quinnjohnson303@gmail.com>
2019-11-25 18:40:24 -0700

    Update Run_Inning and compare simulation expected runs to baseball prospectus' expectations

--- Run_Inning.R
+++ Run_Inning.R
@@ -2,173 +2,6 @@ source("Fix_Pitching.R")
 #Inning data in format of home_team, home_score, vis_team, vis_score, inning, 
 #topbot, outs, runners_pos, home_pitch, vis_pitch
 #n = number of simulations
-LEAGUE_AVG$p1B
-inning_test <- c(1,000,0)
-
-inning <- function(n, bases, outs){
-  runs <- rep(NA, n)
-  for(i in 1:n){
-    runs[i] <- 0
-    while(outs < 3){
-      if(1 == rbinom(1,1, prob = LEAGUE_AVG$p1B)){
-        if(bases == 000){
-          bases <- 001
-        }
-        else if(bases == 001){
-          bases <- 101
-        }
-        else if(bases == 010){
-          bases <- 001
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 100){
-          bases <- 001
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 011){
-          bases <- 101
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 101){
-          bases <- 101
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 110){
-          bases <- 001
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 111){
-          bases <- 001
-          runs[i] <- runs[i] + 2
-        }
-      }
-      else if(1 == rbinom(1,1, prob = LEAGUE_AVG$p2B)){
-        if(bases == 000){
-          bases <- 010
-        }
-        else if(bases == 001){
-          bases <- 010
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 010){
-          bases <- 010
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 100){
-          bases <- 010
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 011){
-          bases <- 010
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 101){
-          bases <- 010
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 110){
-          bases <- 010
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 111){
-          bases <- 010
-          runs[i] <- runs[i] + 3
-        }
-      }
-      else if(1 == rbinom(1,1, prob = LEAGUE_AVG$p3B)){
-        if(bases == 000){
-          bases <- 100
-        }
-        else if(bases == 001){
-          bases <- 100
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 010){
-          bases <- 100
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 100){
-          bases <- 100
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 011){
-          bases <- 100
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 101){
-          bases <- 100
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 110){
-          bases <- 100
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 111){
-          bases <- 100
-          runs[i] <- runs[i] + 3
-        }
-      }
-      else if(1 == rbinom(1,1, prob = LEAGUE_AVG$pHR)){
-        if(bases == 000){
-          runs[i] <- runs[i] + 1
-        }
-        else if(bases == 001){
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 010){
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 100){
-          runs[i] <- runs[i] + 2
-        }
-        else if(bases == 011){
-          runs[i] <- runs[i] + 3
-        }
-        else if(bases == 101){
-          runs[i] <- runs[i] + 3
-        }
-        else if(bases == 110){
-          runs[i] <- runs[i] + 3
-        }
-        else if(bases == 111){
-          runs[i] <- runs[i] + 4
-        }
-      }
-      else if(1 == rbinom(1,1, prob = LEAGUE_AVG$pBB)){
-        if(bases == 000){
-          bases <- 001
-        }
-        else if(bases == 001){
-          bases <- 011
-        }
-        else if(bases == 010){
-          bases <- 011
-        }
-        else if(bases == 100){
-          bases <- 101
-        }
-        else if(bases == 011){
-          bases <- 111
-        }
-        else if(bases == 101){
-          bases <- 111
-        }
-        else if(bases == 110){
-          bases <- 111
-        }
-        else if(bases == 111){
-          bases <- 111
-          runs[i] <- runs[i] + 1
-        }
-      }
-      else{
-        outs <- outs + 1
-      }
-    }
-  }
-  runs
-}
-
 
 inning <- function(n, ibases, iouts){
   runs <- rep(NA, n)
@@ -177,6 +10,7 @@ inning <- function(n, ibases, iouts){
   #  return(inningNum)
   #  for(inning in 1:n){
       #inning <- inning + 1
+      #Initializing values that are tracked throughout the inning
       play <- 0
       outcome <- rep()
       outs <- iouts
@@ -184,6 +18,8 @@ inning <- function(n, ibases, iouts){
       bases <- ibases
       while(outs < 3){
         #print(paste0("outs = ", outs))
+        #Simulating the odds of a type of hit, walk or out
+        #Based on league average probilities
         play <- play + 1
         outcome <- c(outcome, NA)
         outcome[play] <-  sample(c("Out","1B", "2B", "3B", "HR", "BB"), 1, 
@@ -368,18 +204,61 @@ inning <- function(n, ibases, iouts){
   #return(paste0("runs = ", runs))
   runs
 }
-gameScenB <- c("000", "001", "010","100","011", "110", "101", "111")
-gameScenO <- c("Runners", "0", "0", "1", "1", "2", "2")
+
+#Creating dataframe to track how the simulation compares to the prospectus
+#in runs expected given the different game scenarios
 gameScenDF <- rbind(rep(NA,6),rep(NA,6),rep(NA,6),rep(NA,6),rep(NA,6),rep(NA,6),rep(NA,6),rep(NA,6))
-colnames(gameScenDF) <- c("0", "0", "1", "1", "2", "2")
-row.names(gameScenDF) <- c("000", "001", "010","100","011", "110", "101", "111")
+colnames(gameScenDF) <- c("0 - Pros", "0 - Model", "1 - Pros", "1 - Model", "2 - Pros", "2- Model")
+row.names(gameScenDF) <- c("000", "001", "010","100","011", "101", "110", "111")
+#Function to run an inning given the conditions
 gameScen <- function(n,b,o){
   sim <- inning(n,b,o)
-  res <- sum(sim)/n
-  gameScenDF[]
-  return(res)
+  expRun <- sum(sim)/n
+  return(expRun)
 }
-gameScen(10000,111,0)
+#Prospectus
+gameScenDF[,1] <- c(0.5439, 0.9345, 1.1465, 1.3685, 1.4371, 1.7591, 1.9711, 2.3618)
+gameScenDF[,3] <- c(0.2983, 0.5641, 0.7134, 0.9528, 0.9792, 1.2186, 1.3679, 1.6337)
+gameScenDF[,5] <- c(0.1147, 0.2422, 0.3391, 0.3907, 0.4666, 0.5182, 0.6151, 0.7426)
+#Simulating each of the 24 scenarios 1 million times
+gameScenDF[1,2] <- gameScen(1000000, 000, 0)
+gameScenDF[2,2] <- gameScen(1000000, 001, 0)
+gameScenDF[3,2] <- gameScen(1000000, 010, 0)
+gameScenDF[4,2] <- gameScen(1000000, 100, 0)
+gameScenDF[5,2] <- gameScen(1000000, 011, 0)
+gameScenDF[6,2] <- gameScen(1000000, 101, 0)
+gameScenDF[7,2] <- gameScen(1000000, 110, 0)
+gameScenDF[8,2] <- gameScen(1000000, 111, 0)
+gameScenDF[1,4] <- gameScen(1000000, 000, 1)
+gameScenDF[2,4] <- gameScen(1000000, 001, 1)
+gameScenDF[3,4] <- gameScen(1000000, 010, 1)
+gameScenDF[4,4] <- gameScen(1000000, 100, 1)
+gameScenDF[5,4] <- gameScen(1000000, 011, 1)
+gameScenDF[6,4] <- gameScen(1000000, 101, 1)
+gameScenDF[7,4] <- gameScen(1000000, 110, 1)
+gameScenDF[8,4] <- gameScen(1000000, 111, 1)
+gameScenDF[1,6] <- gameScen(1000000, 000, 2)
+gameScenDF[2,6] <- gameScen(1000000, 001, 2)
+gameScenDF[3,6] <- gameScen(1000000, 010, 2)
+gameScenDF[4,6] <- gameScen(1000000, 100, 2)
+gameScenDF[5,6] <- gameScen(1000000, 011, 2)
+gameScenDF[6,6] <- gameScen(1000000, 101, 2)
+gameScenDF[7,6] <- gameScen(1000000, 110, 2)
+gameScenDF[8,6] <- gameScen(1000000, 111, 2)
 
+#Viewing how good of a fit the model 
+prospectus <- c(gameScenDF[,1], gameScenDF[,3], gameScenDF[,5])
+model <- c(gameScenDF[,2],gameScenDF[,4],gameScenDF[,6])
+prosmodel <- lm(model ~ prospectus)
+summary(prosmodel)
+
+cor(prospectus, model)
+#Correlation = 0.987 (good)
+prosmodel$coefficients
+#Slope = 0.851 (should be closer to 1)
+#Intercept = 0.088 (should be closer to 0)
+
+ggplot(prosmodel, aes(x = prospectus, y = model)) + geom_point() +
+  geom_smooth(method = 'lm') + title(xlab = "Prospectus Expected Runs", ylab = "Model Expected Runs")
 
 
